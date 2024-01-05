69e71662c9d14f9010725e8d4b51671bf681c839
agungsurya <100355392+asurya-gz@users.noreply.github.com>
2024-01-05 18:02:56 +0700

    Update page.js

--- src/app/Apoteker/Obat/Rekap/page.js
+++ src/app/Apoteker/Obat/Rekap/page.js
@@ -11,13 +11,14 @@ export default function ApotekerObatRekap() {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [obatList, setObatList] = useState([]);
+  const [filterValue, setFilterValue] = useState("Semua");
 
   // Mengambil data obat
   useEffect(() => {
     // Fetch the list of drugs
     const fetchObatList = async () => {
       try {
-        const response = await axios.get("https://bekk.up.railway.app/obat");
+        const response = await axios.get("http://localhost:4000/obat");
         console.log("Obat list:", response.data.obat);
         setObatList(response.data.obat);
       } catch (error) {
@@ -34,7 +35,7 @@ export default function ApotekerObatRekap() {
     // Fetch user details or check session status
     const fetchUser = async () => {
       try {
-        const response = await axios.get("https://bekk.up.railway.app/user", {
+        const response = await axios.get("http://localhost:4000/user", {
           withCredentials: true,
         });
         console.log("User data:", response.data);
@@ -63,9 +64,15 @@ export default function ApotekerObatRekap() {
   // Print
   const handlePrint = async () => {
     try {
-      // Fetch the list of drugs before printing
-      const response = await axios.get("https://bekk.up.railway.app/obat");
-      const obatListForPrint = response.data.obat;
+      // Fetch the list of drugs based on the selected filter
+      const response = await axios.get("http://localhost:4000/obat");
+      const obatListForPrint = response.data.obat.filter((obat) => {
+        if (filterValue === "Semua") {
+          return true;
+        } else {
+          return handleSaranPembelian(obat.jumlah) === filterValue;
+        }
+      });
 
       // Open a new window for printing
       const printWindow = window.open("", "_blank");
@@ -75,98 +82,98 @@ export default function ApotekerObatRekap() {
 
       // Add your table content to the print document with inline styles
       printDocument.write(`
-        <html>
-          <head>
-            <title>Rekap Obat</title>
-          </head>
-          <body style="
-            font-family: Arial, sans-serif;
+      <html>
+        <head>
+          <title>Rekap Obat</title>
+        </head>
+        <body style="
+          font-family: Arial, sans-serif;
+        ">
+          <div style="
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 1em;
+            overflow-x: auto;
           ">
-            <div style="
+            <!-- Image for letterhead -->
+            <img src="/kop.png" alt="Letterhead" style="width: 100%; max-height: 150px; object-fit: cover; margin-bottom: 5px;">
+
+            <table style="
               width: 100%;
               border-collapse: collapse;
-              margin-top: 1em;
-              overflow-x: auto;
             ">
-              <!-- Image for letterhead -->
-              <img src="/kop.png" alt="Letterhead" style="width: 100%; max-height: 150px; object-fit: cover; margin-bottom: 5px;">
-
-              <table style="
-                width: 100%;
-                border-collapse: collapse;
-              ">
-                <thead>
-                  <tr style="
-                    background-color: #333;
-                    color: white;
+              <thead>
+                <tr style="
+                  background-color: #333;
+                  color: white;
+                ">
+                  <th style="
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    text-align: left;
                   ">
-                    <th style="
-                      border: 1px solid #ddd;
-                      padding: 8px;
-                      text-align: left;
-                    ">
-                      Id
-                    </th>
-                    <th style="
-                      border: 1px solid #ddd;
-                      padding: 8px;
-                      text-align: left;
-                    ">
-                      Nama
-                    </th>
-                    <th style="
-                      border: 1px solid #ddd;
-                      padding: 8px;
-                      text-align: left;
-                    ">
-                      Jumlah 
-                    </th>
-                    <th style="
-                      border: 1px solid #ddd;
-                      padding: 8px;
-                      text-align: left;
-                    ">
-                      Saran Pembelian
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  ${obatListForPrint
-                    .map(
-                      (obat) => `
-                        <tr style="
-                          background-color: #fff;
-                        ">
-                          <td style="
-                            border: 1px solid #ddd;
-                            padding: 8px;
-                            text-align: left;
-                          ">${obat.id}</td>
-                          <td style="
-                            border: 1px solid #ddd;
-                            padding: 8px;
-                            text-align: left;
-                          ">Rp. ${obat.nama_obat}</td>
-                          <td style="
-                            border: 1px solid #ddd;
-                            padding: 8px;
-                            text-align: left;
-                          ">${obat.jumlah}</td>
-                          <td style="
-                            border: 1px solid #ddd;
-                            padding: 8px;
-                            text-align: left;
-                          ">${handleSaranPembelian(obat.jumlah)}</td>
-                        </tr>
-                      `
-                    )
-                    .join("")}
-                </tbody>
-              </table>
-            </div>
-          </body>
-        </html>
-      `);
+                    Id
+                  </th>
+                  <th style="
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    text-align: left;
+                  ">
+                    Nama
+                  </th>
+                  <th style="
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    text-align: left;
+                  ">
+                    Jumlah 
+                  </th>
+                  <th style="
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    text-align: left;
+                  ">
+                    Saran Pembelian
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                ${obatListForPrint
+                  .map(
+                    (obat) => `
+                      <tr style="
+                        background-color: #fff;
+                      ">
+                        <td style="
+                          border: 1px solid #ddd;
+                          padding: 8px;
+                          text-align: left;
+                        ">${obat.id}</td>
+                        <td style="
+                          border: 1px solid #ddd;
+                          padding: 8px;
+                          text-align: left;
+                        ">${obat.nama_obat}</td>
+                        <td style="
+                          border: 1px solid #ddd;
+                          padding: 8px;
+                          text-align: left;
+                        ">${obat.jumlah}</td>
+                        <td style="
+                          border: 1px solid #ddd;
+                          padding: 8px;
+                          text-align: left;
+                        ">${handleSaranPembelian(obat.jumlah)}</td>
+                      </tr>
+                    `
+                  )
+                  .join("")}
+              </tbody>
+            </table>
+          </div>
+        </body>
+      </html>
+    `);
 
       // Close the document and initiate printing
       printDocument.close();
@@ -176,6 +183,7 @@ export default function ApotekerObatRekap() {
     }
   };
   // Print end
+
   return (
     <div className="h-screen">
       {" "}
@@ -193,6 +201,19 @@ export default function ApotekerObatRekap() {
         Cetak Rekap Obat
       </Button>
       {/* Cetak rekap Obat end */}
+      {/* Filter */}
+      <div className="mt-8 ml-4">
+        <select
+          value={filterValue}
+          onChange={(e) => setFilterValue(e.target.value)}
+          className="bg-[#333] text-white p-1 rounded-xl text-center outline-none cursor-pointer"
+        >
+          <option value="Semua">Semua</option>
+          <option value="Beli Obat">Beli Obat</option>
+          <option value="Obat Tersedia">Obat Tersedia</option>
+        </select>
+      </div>
+      {/* Filter end */}
       {/* Table rekap */}
       <div className="overflow-x-auto mt-8">
         <Table hoverable>
@@ -211,14 +232,22 @@ export default function ApotekerObatRekap() {
             </Table.HeadCell>
           </Table.Head>
           <Table.Body>
-            {obatList.map((obat) => (
-              <Table.Row key={obat.id}>
-                <Table.Cell>{obat.id}</Table.Cell>
-                <Table.Cell>{obat.nama_obat}</Table.Cell>
-                <Table.Cell>{obat.jumlah}</Table.Cell>
-                <Table.Cell>{handleSaranPembelian(obat.jumlah)}</Table.Cell>
-              </Table.Row>
-            ))}
+            {obatList
+              .filter((obat) => {
+                if (filterValue === "Semua") {
+                  return true;
+                } else {
+                  return handleSaranPembelian(obat.jumlah) === filterValue;
+                }
+              })
+              .map((obat) => (
+                <Table.Row key={obat.id}>
+                  <Table.Cell>{obat.id}</Table.Cell>
+                  <Table.Cell>{obat.nama_obat}</Table.Cell>
+                  <Table.Cell>{obat.jumlah}</Table.Cell>
+                  <Table.Cell>{handleSaranPembelian(obat.jumlah)}</Table.Cell>
+                </Table.Row>
+              ))}
           </Table.Body>
         </Table>
       </div>
