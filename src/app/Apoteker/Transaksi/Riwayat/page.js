0eb0567f9f9f246719a5590a74ff4f650503852e
agungsurya <100355392+asurya-gz@users.noreply.github.com>
2024-01-05 17:54:27 +0700

    Update page.js

--- src/app/Apoteker/Transaksi/Riwayat/page.js
+++ src/app/Apoteker/Transaksi/Riwayat/page.js
@@ -16,9 +16,7 @@ export default function ApotekerTransaksiRiwayat() {
   useEffect(() => {
     const fetchTransaksiData = async () => {
       try {
-        const response = await axios.get(
-          "https://bekk.up.railway.app/datatransaksi"
-        );
+        const response = await axios.get("http://localhost:4000/datatransaksi");
         console.log("Transaksi data:", response.data);
 
         // Make sure response.data.transaksi is defined and is an array
@@ -40,7 +38,7 @@ export default function ApotekerTransaksiRiwayat() {
     // Fetch user details or check session status
     const fetchUser = async () => {
       try {
-        const response = await axios.get("https://bekk.up.railway.app/user", {
+        const response = await axios.get("http://localhost:4000/user", {
           withCredentials: true,
         });
         console.log("User data:", response.data);
@@ -78,9 +76,7 @@ export default function ApotekerTransaksiRiwayat() {
   const handlePrintRiwayat = async () => {
     try {
       // Fetch the list of transactions based on the filter
-      const response = await axios.get(
-        "https://bekk.up.railway.app/datatransaksi"
-      );
+      const response = await axios.get("http://localhost:4000/datatransaksi");
       const transaksiListForPrint = response.data.transaksi;
 
       // Filter the transactions based on the selected filter option
